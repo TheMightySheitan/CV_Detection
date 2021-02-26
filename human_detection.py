0c9acd16d5b996b531b85ec3f31b5620ea08781f
Pierre Ferrari <p.ferrari@flymetothecloud.fr>
2021-02-26 23:58:30 +0100

    Adding some detections routines

--- human_detection.py
+++ human_detection.py
@@ -11,11 +11,17 @@ import argparse
 HOGCV = cv2.HOGDescriptor()
 HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+"""
+detect(frame) lists containing coordinates of bounding box of person through the function detectMultiScale()
+Coordinates are x, y, w, h, where x,y are the starting coordinates of the box, w,h are the width and height of the given box 
+"""
+
+
 def detect(frame):
     bounding_box_coordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
     person = 1
     for x, y, w, h in bounding_box_coordinates:
-        cv2.rectangle(frame, (x, y), (x+y+w+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + y + w + h), (0, 255, 0), 2)
         cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
         person += 1
 
@@ -24,3 +30,23 @@ def detect(frame):
     cv2.imshow('output', frame)
 
     return frame
+
+
+def humanDetector(args):
+    image_path = args["image"]
+    video_path = args['video']
+    if str(args["camera"]):
+        camera = True
+    else:
+        camera = False
+
+    writer = None
+    if args['output'] is not None and image_path is None:
+        writer = cv2.VideoWriter(args['output'], cv2.VideoWriter_fourcc(*'MPJG'), 10, (600, 600))
+
+    if camera:
+        print('[INFO] Opening camera')
+
+
+def detectByCamera(writer):
+    video = cv2.VideoCapture(0)
