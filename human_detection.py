c6d9a6ea18d69e67dbf4a796d5b033c27fcf387d
TheMightySheitan <70757207+TheMightySheitan@users.noreply.github.com>
2021-03-05 14:41:15 +0100

    Adding more methods and main function

--- human_detection.py
+++ human_detection.py
@@ -46,7 +46,90 @@ def humanDetector(args):
 
     if camera:
         print('[INFO] Opening camera')
+        detectByCamera(output_path, writer)
+    elif video_path is not None:
+        print('[INFO] Opening video from path')
+        detectByPathVideo(video_path, writer)
+    elif image_path is not None:
+        print('[INFO] Opening image from path')
+        detectByPathImage(image_path, args['output'])
 
 
 def detectByCamera(writer):
     video = cv2.VideoCapture(0)
+    """
+    VideoCapture(0) : uses the default camera on the running device
+    """
+    print('Detecting people...')
+
+    while True:
+        check, frame = video.read()
+
+        frame = detect(frame)
+        if writer is not None:
+            writer.write(frame)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+
+def detectByPathVideo(path, writer):
+    video = cv2.VideoCapture(path)
+    check, frame = video.read()
+    if not check:
+        print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
+        return
+    print('Detecting people...')
+    while video.isOpened():
+        # check is True if reading was successful
+        check, frame = video.read()
+        if check:
+            frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+            frame = detect(frame)
+
+            if writer is not None:
+                writer.write(frame)
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+        else:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+
+"""
+this method is used if a person needs to be detected from an image
+"""
+
+
+def detectByPathImage(path, output_path):
+    image = cv2.imread(path)
+    image = imutils.resize(image, width=min(800, image.shape[1]))
+    result_image = detect(image)
+    if output_path is not None:
+        cv2.imwrite(output_path, result_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def argsParser():
+    arg_parse = argparse.ArgumentParser()
+    arg_parse.add_argument("-v", "--video", default=None, help="path to Video File ")
+    arg_parse.add_argument("-i", "--image", default=None, help="path to Image File ")
+    arg_parse.add_argument("-c", "--camera", default=False, help="Set true if you want to use the camera.")
+    arg_parse.add_argument("-o", "--output", type=str, help="path to optional output video file")
+    args = vars(arg_parse.parse_args())
+    return args
+
+
+if __name__ == "__main__":
+    HOGCV = cv2.HOGDescriptor()
+    HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    args = argsParser()
+    humanDetector(args)
